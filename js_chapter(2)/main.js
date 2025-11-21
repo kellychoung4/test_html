d69d8cb8129b7b38448cb8045e258691948072a4
kellychoung4 <kelly.choung@gmail.com>
2025-11-20 22:16:01 -0800

    Lab + Assignment (Chapter 2)

---
+++ js_chapter(2)/main.js
@@ -0,0 +1,54 @@
+// declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
+let cats = 22;
+
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+
+let dogs = 13;
+
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
+
+
+let addingVariables = cats + dogs;
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!'
+
+let addingStrings = "javascript is" + "really awesome!" ;
+
+
+
+
+
+
+// declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
+
+let myArray = [ "Coding is awesome", 500, false ];
+
+// resassign myArray[1] to 250
+
+myArray[1] = 200;
+
+// create your own variable and assign a random string/message to it
+
+let message = "JavaScript is so fun!!";
+let classA = ["Drawing1", "T/TH", 18];
+let classB = ["WaterColor-B", "F", 10];
+let classC = ["Photoshop", "M/W", 12];
+
+let totalStudent = classA[2] + classB[2] + classC[2] ;
